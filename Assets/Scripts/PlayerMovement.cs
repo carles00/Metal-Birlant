91fb7c3e1e661cd66213a49fa10f52a3916b55db
Carles Gallardo <45995898+carles00@users.noreply.github.com>
2023-02-16 11:44:22 +0100

    variable jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,39 +14,70 @@ public class PlayerMovement : MonoBehaviour
     [Range(1f, 10f)][SerializeField] private float jumpForce;
     [Range(0, .3f)] [SerializeField] private float acceleration;
     [SerializeField] private LayerMask colliderMask;
+    [SerializeField] private Transform groundCheck;
 
     private Rigidbody2D rigidBody;
-    private BoxCollider2D collider;
+    private BoxCollider2D boxCollider;
     private Vector3 velocity = Vector3.zero;
 
     bool grounded;
     float move;
     bool jump = false;
+    bool isJumping = false;
+    float jumpAcceleration = 10f;
   
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<BoxCollider2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
  
     private void FixedUpdate()
     {   
+        checkGround();
 
+        movement();
+        Debug.Log(isJumping);
     }
 
     private void movement(){
-
+        if(jump && grounded)
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+        else if(isJumping && !jump)
+        {
+            rigidBody.AddForce(Vector2.up * jumpAcceleration, ForceMode2D.Force);
+        }
     }
 
-    private bool isGrounded(){
+    private void checkGround(){
 
-        return false;
+        
+        grounded = Physics2D.Raycast(groundCheck.position, Vector2.down, .2f, colliderMask);
+        Color rayColor = grounded ? Color.red : Color.green;
+        Debug.DrawRay(groundCheck.position, Vector2.down * .2f, rayColor);
+
     }
 
     public void onJump(InputAction.CallbackContext context)
     {
-        jump = true;
+        if (context.started)
+        {
+            jump = true;
+            isJumping = true;
+        }
+        if (context.canceled)
+        {
+            isJumping = false;
+        }
+        if (context.performed)
+        {
+            isJumping = false;
+        }
+        
     }
 
     public void onMove(InputAction.CallbackContext context)
@@ -55,6 +86,4 @@ public class PlayerMovement : MonoBehaviour
         move = context.ReadValue<Single>() * runSpeed;
         
     }
-
-
 }
