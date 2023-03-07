e07f0e3d0fb48a01e7db06e18b9b729a67274c43
Carles Gallardo <45995898+carles00@users.noreply.github.com>
2023-03-07 10:48:47 +0100

    refactored input system

--- Assets/Scripts/GameControllerScripts/CameraController.cs
+++ Assets/Scripts/GameControllerScripts/CameraController.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform playerPosition;
     [Range(-5f, 5f)] [SerializeField] float offset = 0;
+    [SerializeField] private bool folowPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        folowPlayer = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(0, playerPosition.position.y + offset, -10);
+        if(folowPlayer) {
+            transform.position = new Vector3(0, playerPosition.position.y + offset, -10);
+        }
+    }
+
+    public void SwitchPlayerFollowing()
+    {
+        folowPlayer = !folowPlayer;
+    }
+
+    public void OnScroll(InputAction.CallbackContext ctx)
+    {
+        Debug.Log(ctx);
     }
 }
--- Assets/Scripts/GameControllerScripts/GameController.cs
+++ Assets/Scripts/GameControllerScripts/GameController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Transform playerSpawn;
-    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject presentPlayer;
+    [SerializeField] private GameObject futurePlayer;
 
     
     [SerializeField] private int playerLive = 3;
@@ -20,9 +24,25 @@ public class GameController : MonoBehaviour
     [SerializeField] private int round = 0;
     [SerializeField] private int currentTurn = 0;
 
+    [Header("Camera")]
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private int presentFov = 60;
+    [SerializeField] private int futurePov = 90;
+    private CameraController cameraController;
+
+    [Header("Future")]
+    [SerializeField] private GameObject futureInterface;
+
+    private PlayerInput input;
+    
+
     void Start()
     {
-        player.transform.position = playerSpawn.position;
+        cameraController = mainCamera.GetComponent<CameraController>();
+        input = GetComponent<PlayerInput>();
+        presentPlayer.transform.position = playerSpawn.position;
+        futureInterface.SetActive(false);
+        futurePlayer.SetActive(false);
     }
 
     // Update is called once per frame
@@ -36,9 +56,32 @@ public class GameController : MonoBehaviour
 
     public void TreasureReached()
     {
-        Debug.Log("Treasure from game controller");
+        ChangeToFuture();
         //Give money to player
         //Change to future
+    }
 
+    private void ChangeToFuture()
+    {
+        presentPlayer.SetActive(false);
+        futurePlayer.SetActive(true);
+        futureInterface.SetActive(true);
+
+        mainCamera.fieldOfView = futurePov;
+
+        cameraController.SwitchPlayerFollowing();
+        input.SwitchCurrentActionMap("UI");
+    }
+
+    private void ChangeToPresent()
+    {
+        cameraController.SwitchPlayerFollowing();
+        presentPlayer.transform.position = playerSpawn.position;
+        mainCamera.fieldOfView = presentFov;
+        input.SwitchCurrentActionMap("Player");
+        presentPlayer.SetActive(true);
+        futurePlayer.SetActive(false);
+        futureInterface.SetActive(false);
+        
     }
 }
