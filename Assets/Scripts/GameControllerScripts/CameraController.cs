32b63cac234391a02aed08f7039566f222edd3f0
Carles Gallardo <45995898+carles00@users.noreply.github.com>
2023-03-07 11:34:17 +0100

    improved camera movement

--- Assets/Scripts/GameControllerScripts/CameraController.cs
+++ Assets/Scripts/GameControllerScripts/CameraController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,19 +10,31 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform playerPosition;
     [Range(-5f, 5f)] [SerializeField] float offset = 0;
     [SerializeField] private bool folowPlayer;
+    [SerializeField][Range(0,0.5f)] private float lerpStep = 0.05f;
+    [SerializeField] private Vector3 cameraTarget;
+    [SerializeField] private float scrollDistance = 5;
+    [SerializeField] private float maxHeight = 0;
+    [SerializeField] private float minHeight = -140;
 
     // Start is called before the first frame update
     void Start()
     {
         folowPlayer = true;
+        cameraTarget = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(folowPlayer) {
-            transform.position = new Vector3(0, playerPosition.position.y + offset, -10);
+        if (folowPlayer)
+        {
+            cameraTarget = playerPosition.position;
         }
+        
+        float lerpedPosition = Mathf.Lerp(transform.position.y, cameraTarget.y, lerpStep);
+        float clampedPosition = Mathf.Clamp(lerpedPosition, minHeight, maxHeight);
+        transform.position = new Vector3(0, clampedPosition, transform.position.z);
+
     }
 
     public void SwitchPlayerFollowing()
@@ -30,6 +44,17 @@ public class CameraController : MonoBehaviour
 
     public void OnScroll(InputAction.CallbackContext ctx)
     {
-        Debug.Log(ctx);
+
+        if (ctx.performed)
+        {
+            float value = ctx.ReadValue<Vector2>().y;
+            if(Mathf.Abs(value) > 0.1f) {
+                float scrollDirection = Mathf.Sign(value);
+                Debug.Log(scrollDirection);
+                cameraTarget = new Vector3(0,transform.position.y + (scrollDirection * scrollDistance),transform.position.z);
+            }
+            
+        }
+        
     }
 }
